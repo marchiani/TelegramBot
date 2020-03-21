cd8ceec9aee9fb3cc99315d2ae13e27d5c055b06
Dmytro Boychenko <marchiani22@gmail.com>
2020-03-21 13:00:54 +0200

    New project on .NET CORE 3

---
+++ CodeAriumTelegramBot/Configuration.cs
@@ -0,0 +1,7 @@
+namespace Telegram.Bot.Examples.Echo
+{
+    public static class Configuration
+    {
+        public readonly static string BotToken = "1133650379:AAFp_TD2eZemWCFTIlPf0niCcW1nR7G36VQ";
+    }
+}
