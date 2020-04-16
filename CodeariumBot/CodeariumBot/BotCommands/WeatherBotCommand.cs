513771bdc34b8794851baa902d497bcd60b1da78
Dmytro Boychenko <marchiani22@gmail.com>
2020-04-16 17:35:33 +0300

    CA - Initial projects for bot Added changes

--- CodeariumBot/CodeariumBot/BotCommands/WeatherBotCommand.cs
+++ CodeariumBot/CodeariumBot/BotCommands/WeatherBotCommand.cs
@@ -2,19 +2,15 @@
 using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.ReplyMarkups;
-using WeatherNet.Clients;
-using WeatherNet.Model;
 
 namespace CodeariumBot.BotCommands
 {
     public class WeatherBotCommand
     {
-        private readonly CurrentWeather CurrentWeather;
         private readonly TelegramBotClient TelegramBotClient;
         private readonly Update Update;
         public WeatherBotCommand(TelegramBotClient telegramBotClient, Update update)
         {
-            CurrentWeather = new CurrentWeather();
             TelegramBotClient = telegramBotClient;
             Update = update;
         }
@@ -34,20 +30,16 @@ namespace CodeariumBot.BotCommands
         public async Task WeatherForecast(Location location)
         {
 
-            var weather = CurrentWeather.GetByCityName("Kiev", "Ukrain");
-            var forecast = FiveDaysForecast.GetByCoordinates(location.Latitude, location.Longitude);
-
 
             await TelegramBotClient.SendTextMessageAsync(
                 chatId: Update.Message.Chat.Id,
-                text: $"{weather.Item.Temp}"
+                text: $"{""}"
             );
 
-            var weather1 = CurrentWeather.GetByCoordinates(location.Latitude, location.Longitude);
 
             await TelegramBotClient.SendTextMessageAsync(
                 chatId: Update.Message.Chat.Id,
-                text: $"{weather1.Item.Temp}"
+                text: $"{""}"
             );
         }
     }
--- CodeariumBot/CodeariumServices/Interfaces/IWeather.cs
+++ CodeariumBot/CodeariumServices/Interfaces/IWeather.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CodeariumServices.Interfaces
+﻿namespace CodeariumServices.Interfaces
 {
-	class IWeather
+	public interface IWeather
 	{
+		void GetCurrentWeatherByCoordinats();
 	}
 }
